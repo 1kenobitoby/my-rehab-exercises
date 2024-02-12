f6b95f74a4c023023aa84ca7cc04698e035e8081
1kenobitoby <lenehto@aol.com>
2024-02-12 17:21:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Rehab exercises app
+An app that serves me up a daily reminder of what rehab exercises I'm supposed to be doing that day.
+
+# Project description
+This is a Streamlit web app written in Python 3.8.10. It\'s intended purely for my own personal use to serve me up a daily random selection of injury rehab exercises to do that day.<br>
+The project has been built in Streamlit V1.27.0 and distributed as a public app on the Streamlit Community Cloud.<br>
+There are no plans to develop or update the app Copy the code and adapt if freely for your personal needs if it would be useful to you<br>
+
+# How to run the project
+Open the URL [https://my-rehab-exercises.streamlit.app](https://my-rehab-exercises.streamlit.app)
+
+# Dependencies
+streamlit >=1.27.0<br>
+pandas >=1.5.2<br>
+
+# Licence
+The code is copyrighted and no specific licence for its use or distribution is granted. That said, users are welcome to inspect the code, clone the repository, copy code snippets and adapt the code to their own purposes if doing so would help them solve problems with their own projects.
---
+++ my-rehab-exercises.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import time
+import random
+import pandas as pd
+
+# Get struct_time and strip full weekday name from it (%A)
+day=time.strftime("%A", time.localtime())
+
+
+
+st.header(day + "'s exercises")
+
+# a dictionary of 3 lists: 1. Exercise names; 2. Video URLs showing how to perform the exercises; 3. The start time in seconds of the relevant bit in the video
+database={
+    'Exercise name': ["Curl up", "Bird dog", "Side plank", "Knee off bed", "Scorpion roll", "SI joint corkscrew", "Windscreen/piriformis rolls", "Tennis ball release", "Donkey kicks", "90:90 sacrum lifts", "Wall sits", "Single leg squats", "Clam shells", "Fire hydrants", "Bridges/thrusts/donkey kicks", "SL abduction", "90 wall ball roll", "Ball-wall deadlift"],
+    'Video':["https://www.youtube.com/watch?v=2_e4I-brfqs", "https://www.youtube.com/watch?v=2_e4I-brfqs", "https://www.youtube.com/watch?v=2_e4I-brfqs", "https://www.youtube.com/watch?v=W8aPYm-CHIk", "https://www.youtube.com/watch?v=W8aPYm-CHIk", "https://www.youtube.com/watch?v=W8aPYm-CHIk", "https://www.youtube.com/watch?v=W8aPYm-CHIk", "https://www.youtube.com/watch?v=7KYfGH8daJk", "https://www.youtube.com/watch?v=7KYfGH8daJk", "https://www.youtube.com/watch?v=Z-J6rSUpmN4", "https://www.youtube.com/watch?v=Z-J6rSUpmN4", "https://www.youtube.com/watch?v=Z-J6rSUpmN4", "https://www.youtube.com/watch?v=IcmDsB3wdEk", "https://www.youtube.com/watch?v=IcmDsB3wdEk", "https://www.youtube.com/watch?v=IcmDsB3wdEk", "https://www.youtube.com/watch?v=IcmDsB3wdEk", "https://www.youtube.com/watch?v=Qhk6nrorgT8", "https://www.youtube.com/watch?v=Qhk6nrorgT8"],
+    'Start time':[68, 295, 205, 58, 323, 446, 603, 71, 524, 169, 377, 686, 474, 490, 505, 524, 279, 396]
+}
+
+# Pass database to pandas dataframe constructor to make 'table'
+# Table has a row for each exercise showing name, video URL and start time in video
+table = pd.DataFrame(database)
+
+#table
+
+# pick 6 rows at random from 18 and put in list 'exercises'
+exercises = random.sample(range(0,18), 6)
+#exercises
+
+#initialise counter 'number'
+number=0
+
+# Loop through pulling the exercise name from column 1 to put in the Streamlit tab called tab_text
+# then pull the associated video URL from column 2 and set the start time from column 3
+for i in exercises:
+    number +=1
+    tab_text, tab_video = st.tabs(["Exercise " + "%.0f" % number, "Video"])
+    with tab_text:
+        st.write(table.iloc[i, 0])
+
+    with tab_video:
+        st.video(table.iloc[i, 1], start_time=table.iloc[i,2]) 
+
+st.divider()
+
+# Some gym workout schedules and YouTube stretching routines, 1 for each day of the week, to follow.
+st.header('Today\'s workout')
+
+if day=="Saturday":
+    st.image('images/day_1.png')
+if day=="Sunday":
+    st.vidoe("https://www.youtube.com/watch?v=0arLyM00F_w")    
+elif day=="Monday":
+    st.video('https://www.youtube.com/watch?v=FX-R98zU5MI')
+elif day=="Tuesday":
+    st.image('images/day_2.png')
+elif day=="Wendesday":
+    st.image('https://www.youtube.com/watch?v=ui3ToKZtKIs', start_time=30)
+elif day=="Thursday":
+    st.image('images/day_3.png')
+else:
+    st.video('https://www.youtube.com/watch?v=FX-R98zU5MI')    
+
+
